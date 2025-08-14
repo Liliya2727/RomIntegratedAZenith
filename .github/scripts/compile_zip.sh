39aae2f6d664cd796bdedb7bd53ea959a2d402b7
xia <ridho.holy@gmail.com>
2025-08-14 13:14:27 +0700

    Update compile_zip.sh

--- .github/scripts/compile_zip.sh
+++ .github/scripts/compile_zip.sh
@@ -17,16 +17,16 @@ version_code="$(git rev-list HEAD --count)"
 release_code="$(git rev-parse --short HEAD)-Release"
 
 # Copy module files
-cp -r ./libs modules/hw
+cp -r ./libs/arm64-v8a/vendor.azenith-service modules/bin/hw
 cp -r ./init.azenith.rc modules/init
-cp -r ./tweakfls/* modules/profilersh
+cp -r ./tweakfls/* modules/bin
 cp -r ./vendor_sepolicy.cil modules/
 cp LICENSE ./modules
 cp README.md ./modules
 cp changelog.md ./modules
 
 # Remove .sh extension from scripts
-find modules/profilersh -maxdepth 1 -type f -name "*.sh" -exec sh -c 'mv -- "$0" "${0%.sh}"' {} \;
+find modules/bin -maxdepth 1 -type f -name "*.sh" -exec sh -c 'mv -- "$0" "${0%.sh}"' {} \;
 
 # Parse version info to module prop
 zipName="AZenithRomINT-v$version-$release_code"
