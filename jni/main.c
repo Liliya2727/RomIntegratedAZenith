0a75cbe28c795a8a2bdd36308e2fd177e32152dd
Liliya <syarifbintang16@gmail.com>
2025-08-21 17:55:29 +0700

    add back log interface even tho we never use it

--- jni/main.c
+++ jni/main.c
@@ -26,9 +26,9 @@ int main(int argc, char* argv[]) {
 
     // Expose logging interface for other modules
     char* base_name = basename(argv[0]);
-    if (strcmp(base_name, "sys.azenith-service_log") == 0) {
+    if (strcmp(base_name, "AZenith_log") == 0) {
         if (argc < 3) {
-            fprintf(stderr, "Usage: sys.azenith-service_log <TAG> <LEVEL> <MESSAGE>\n");
+            fprintf(stderr, "Usage: AZenith_log <TAG> <LEVEL> <MESSAGE>\n");
             fprintf(stderr, "Levels: 0=DEBUG, 1=INFO, 2=WARN, 3=ERROR, 4=FATAL\n");
             return EXIT_FAILURE;
         }
@@ -59,12 +59,6 @@ int main(int argc, char* argv[]) {
         return EXIT_SUCCESS;
     }
 
-
-    // Make sure only one instance is running
-    if (check_running_state() == 1) {
-        fprintf(stderr, "\033[31mERROR:\033[0m Another instance of Daemon is already running!\n");
-        exit(EXIT_FAILURE);
-    }
 
     // Register signal handlers
     signal(SIGINT, sighandler);
